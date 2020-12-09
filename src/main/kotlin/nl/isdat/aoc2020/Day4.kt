01896b78c7996c494b8ded7a65438299b551bec3
Robert Horlings <robert@isdat.nl>
2020-12-09 18:29:21 +0100

    Day 4 2020

---
+++ src/main/kotlin/nl/isdat/aoc2020/Day4.kt
@@ -0,0 +1,81 @@
+package nl.isdat.aoc2020
+
+import nl.isdat.aoc2020.Utils.fileAsList
+
+object Day4 {
+    val REQUIRED_FIELDS = listOf(
+        "byr",
+        "iyr",
+        "eyr",
+        "hgt",
+        "hcl",
+        "ecl",
+        "pid",
+        // "cid"
+    )
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val passports = parse(fileAsList("day4.txt"))
+
+        part1(passports)
+        part2(passports)
+    }
+
+    private fun part1(passports: List<Passport>) {
+        println("Part 1 result: " + passports.count { it.containsRequiredFields() })
+    }
+
+    private fun part2(passports: List<Passport>) {
+        println("Part 2 result: " + passports.count { it.containsRequiredFields() && it.matchesAllRules() })
+    }
+
+    private fun parse(lines: List<String>): List<Passport> {
+        var remaining = lines
+        val passports = mutableListOf<Passport>()
+
+        while(remaining.isNotEmpty()) {
+            val passportLines = remaining.takeWhile { it.isNotEmpty() }
+
+            passports += Passport(
+                passportLines
+                    .flatMap {
+                        it.split(" ")
+                            .map {
+                                it.split(":").let { it[0] to it[1] }
+                            }
+                    }
+                    .toMap()
+            )
+
+            remaining = remaining.drop(passportLines.size + 1)
+        }
+
+        return passports
+    }
+
+    data class Passport(val fields: Map<String, String>) {
+        fun containsRequiredFields() = fields.keys.containsAll(REQUIRED_FIELDS)
+        fun matchesAllRules() = rules.all { (key, validator) -> fields[key]?.let(validator) ?: true  }
+
+        val rules: Map<String, (String) -> Boolean> = mapOf(
+            "byr" to { it.toIntOrNull()?.let { it >= 1920 && it <= 2002 } ?: false },
+            "iyr" to { it.toIntOrNull()?.let { it >= 2010 && it <= 2020 } ?: false },
+            "eyr" to { it.toIntOrNull()?.let { it >= 2020 && it <= 2030 } ?: false },
+            "hgt" to {
+                val value = it.takeWhile { it.isDigit() }.toIntOrNull()
+                val unit = it.dropWhile { it.isDigit() }
+
+                when(unit) {
+                    "cm" -> value?.let { it >= 150 && it <= 193 } ?: false
+                    "in" -> value?.let { it >= 59 && it <= 76 } ?: false
+                    else -> false
+                }
+            },
+            "hcl" to { it.matches("""#[0-9a-f]{6}""".toRegex()) },
+            "ecl" to { listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(it) },
+            "pid" to { it.all { it.isDigit() } && it.length == 9 }
+        )
+    }
+}
