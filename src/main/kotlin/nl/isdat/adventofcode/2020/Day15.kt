b9847439530c3965a394c206cb09330d4005cb96
Robert Horlings <robert@isdat.nl>
2020-12-21 20:54:45 +0100

    Day 15 2020

---
+++ src/main/kotlin/nl/isdat/adventofcode/2020/Day15.kt
@@ -0,0 +1,40 @@
+package nl.isdat.adventofcode.`2020`
+
+import nl.isdat.adventofcode.Day
+import nl.isdat.adventofcode.Input.fileAsList
+import nl.isdat.adventofcode.Input.fileAsString
+import java.math.BigInteger
+import kotlin.math.pow
+
+
+class Day15 : Day() {
+    val numbers = fileAsString("2020/day15.txt").split(",").map(String::toInt).toList()
+
+    val game = sequence {
+        // The first turns are the starting numbers
+        yieldAll(numbers)
+
+        // Now keep track of when a number was last seen
+        // Skip the last item from the starting numbers list, as that is the number to consider
+        // in the first iteration
+        val lastSeen = numbers.dropLast(1).mapIndexed { idx, i -> i to idx }.toMap().toMutableMap()
+        var turn = numbers.size - 1
+        var previous = numbers.last()
+
+        while (true) {
+            var newValue = if (!lastSeen.containsKey(previous)) {
+                0
+            } else {
+                turn - lastSeen[previous]!!
+            }
+
+            yield(newValue)
+
+            lastSeen[previous] = turn++
+            previous = newValue
+        }
+    }
+
+    override fun part1() = game.take(2020).last()
+    override fun part2() = game.take(30000000).last()
+}
--- src/main/kotlin/nl/isdat/adventofcode/Input.kt
+++ src/main/kotlin/nl/isdat/adventofcode/Input.kt
@@ -3,7 +3,8 @@ package nl.isdat.adventofcode
 import java.nio.charset.Charset
 
 object Input {
-
-    fun fileAsSequence(filename: String) = javaClass.classLoader.getResourceAsStream(filename).bufferedReader(Charset.defaultCharset()).lineSequence()
+    fun fileAsReader(filename: String) = javaClass.classLoader.getResourceAsStream(filename).bufferedReader(Charset.defaultCharset())
+    fun fileAsSequence(filename: String) = fileAsReader(filename).lineSequence()
     fun fileAsList(filename: String) = fileAsSequence(filename).toList()
+    fun fileAsString(filename: String) = fileAsReader(filename).lines().reduce { a, b -> "${a}\n${b}" }.orElse("")
 }
